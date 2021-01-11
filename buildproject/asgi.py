f94e0df64e702d6fed4bd5ed2140f60e1a273ebf
Hamdi Abidi <mrhamdiabidi@gmail.com>
2021-01-11 21:05:58 +0100

    Initial Commit

---
+++ buildproject/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for buildproject project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'buildproject.settings')
+
+application = get_asgi_application()
